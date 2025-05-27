779da3797d8e123323d776ac14fbab114b49426a
Rich-D2 <richi2@gmail.com>
2025-05-27 18:46:58 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,22 @@ import CustomerDashboard from './components/CustomerDashboard';
 import ParalegalDashboard from './components/ParalegalDashboard';
 import LawyerDashboard from './components/LawyerDashboard';
 import AdminDashboard from './components/AdminDashboard';
+import Login from './components/Login';
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route element={<Layout />}>
           <Route path="/" element={<CustomerDashboard />} />
           <Route path="/customer" element={<CustomerDashboard />} />
           <Route path="/paralegal" element={<ParalegalDashboard />} />
           <Route path="/lawyer" element={<LawyerDashboard />} />
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="*" element={<CustomerDashboard />} />
-        </Routes>
-      </Layout>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
